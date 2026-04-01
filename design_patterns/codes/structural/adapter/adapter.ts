2f87079de13ef45fbc5feb01bac903dff222b1ad
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2026-04-01 14:26:02 +0600

    added adapter.ts

---
+++ design_patterns/codes/structural/adapter/adapter.ts
@@ -0,0 +1,50 @@
+// target or interface
+class Target{
+
+    public request(): string{
+        return "TARGET: Default target behavior";
+    }
+}
+
+// adaptee or service
+class Adaptee{
+    public specificRequest(): string{
+        return ".eetpadA eht fo roivaheb laicepS";
+    }
+}
+
+// adapter
+class Adapter extends Target{
+    private adaptee: Adaptee;
+    constructor(adaptee: Adaptee){
+        super();
+        this.adaptee=adaptee;
+    };
+
+    public request(): string {
+        const result=this.adaptee.specificRequest().split('').reverse().join('');
+
+        return `ADAPTER: TRANSLATED ${result}`;
+    }
+}
+
+// client
+function clientCode(target: Target){
+    console.log(target.request());
+}
+
+console.log('Client: I can work just fine with the Target objects:');
+const target = new Target();
+clientCode(target);
+
+console.log('');
+
+const adaptee = new Adaptee();
+console.log('Client: The Adaptee class has a weird interface. See, I don\'t understand it:');
+console.log(`Adaptee: ${adaptee.specificRequest()}`);
+
+console.log('');
+
+console.log('Client: But I can work with it via the Adapter:');
+const adapter=new Adapter(adaptee);
+clientCode(adapter);
